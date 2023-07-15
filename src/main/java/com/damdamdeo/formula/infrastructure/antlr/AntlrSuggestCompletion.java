a0d3ce83c850f614a6d7f604baa65e2966c7853a
damien <damien.clementdhuart@gmail.com>
2023-07-15 23:33:08 +0200

    add logger

--- src/main/java/com/damdamdeo/formula/infrastructure/antlr/AntlrSuggestCompletion.java
+++ src/main/java/com/damdamdeo/formula/infrastructure/antlr/AntlrSuggestCompletion.java
@@ -6,6 +6,8 @@ import com.damdamdeo.formula.domain.SuggestCompletion;
 import com.damdamdeo.formula.domain.SuggestedFormula;
 import com.damdamdeo.formula.domain.SuggestionsCompletion;
 import com.damdamdeo.formula.infrastructure.antlr.autosuggest.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Comparator;
 import java.util.List;
@@ -13,6 +15,7 @@ import java.util.concurrent.*;
 import java.util.stream.Collectors;
 
 public class AntlrSuggestCompletion implements SuggestCompletion {
+    private static final Logger LOGGER = LogManager.getLogger(AntlrSuggestCompletion.class);
 
     @Override
     public SuggestionsCompletion suggest(final SuggestedFormula suggestedFormula) {
@@ -31,10 +34,13 @@ public class AntlrSuggestCompletion implements SuggestCompletion {
             try {
                 return future.get(5, TimeUnit.SECONDS);
             } catch (final InterruptedException interruptedException) {
+                LOGGER.error(interruptedException);
                 throw new AutoSuggestUnavailableException(suggestedFormula, interruptedException);
             } catch (final ExecutionException executionException) {
+                LOGGER.error(executionException);
                 throw new AutoSuggestionExecutionException(suggestedFormula, executionException.getCause());
             } catch (final TimeoutException timeoutException) {
+                LOGGER.error(timeoutException);
                 future.cancel(true);
                 throw new AutoSuggestionExecutionTimedOutException(suggestedFormula, timeoutException);
             } finally {
