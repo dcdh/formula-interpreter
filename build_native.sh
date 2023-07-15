dfbf7861a73568d084dad3705b1c5f3027d3a79e
damien <damien.clementdhuart@gmail.com>
2023-07-15 22:34:36 +0200

    fix docker build command

--- build_native.sh
+++ build_native.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 mvn clean install verify -P native &&
-  docker build - < src/main/docker/Dockerfile.native-micro -t damdamdeo/formula-interpreter
+  docker build -f src/main/docker/Dockerfile.native-micro -t damdamdeo/formula-interpreter .
