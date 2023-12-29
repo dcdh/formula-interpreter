91e8fe8129ea3cff8e9e874c0fcd132b733f6bca
damien <damien.clementdhuart@gmail.com>
2023-12-29 11:33:01 +0100

    use podman

--- build_native.sh
+++ build_native.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
+export DOCKER_HOST=unix:///run/user/${UID}/podman/podman.sock
+export TESTCONTAINERS_RYUK_DISABLED=true
 mvn clean install verify -P native &&
   docker build -f src/main/docker/Dockerfile.native-micro -t damdamdeo/formula-interpreter .
