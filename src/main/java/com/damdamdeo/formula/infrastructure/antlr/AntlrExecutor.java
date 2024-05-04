cbb6bf4d88a87aef62007a8c5335e10661a98990
damien <damien.clementdhuart@gmail.com>
2024-05-04 21:20:43 +0200

    explain why the current computation of time of execution is broken I should rewrite it by computing the time in each stage

--- src/main/java/com/damdamdeo/formula/infrastructure/antlr/AntlrExecutor.java
+++ src/main/java/com/damdamdeo/formula/infrastructure/antlr/AntlrExecutor.java
@@ -26,23 +26,28 @@ public final class AntlrExecutor implements Executor {
     public Uni<ExecutionResult> execute(final Formula formula,
                                         final StructuredData structuredData,
                                         final ExecutionWrapper executionWrapper) {
-        final ExecutedAtStart executedAtStart = executedAtProvider.now();
-        return antlrParseTreeGenerator.generate(formula)
-                .onItem().transform(AntlrParseTreeGenerator.GeneratorResult::parseTree)
-                .onItem().transformToUni(parseTree ->
-                        Uni.createFrom().item(() -> {
-                            final EvalVisitor visitor = new EvalVisitor(executionWrapper, structuredData, numericalContext);
-                            final Result result = visitor.visit(parseTree);
-                            if (result == null) {
-                                throw new IllegalStateException("Should not be null - a response is expected");
-                            }
-                            final List<ElementExecution> elementExecutions = executionWrapper.executions();
-                            final ExecutedAtEnd executedAtEnd = executedAtProvider.now();
-                            return new ExecutionResult(result,
-                                    elementExecutions,
-                                    new ExecutionProcessedIn(executedAtStart, executedAtEnd));
-                        })
-                )
+        // I would like to do all the treatment in only one block.
+        // However, it is not possible because the cache used regarding the antlrParseTreeGenerator is async and I cannot wait on it
+        // because it is forbidden inside the event loop.
+        // So the time to execute the formula do not reflect the reality because the processing can be hanged by processing
+        // another formula
+        // I would like to have a sync get value from cache, but it is not provided by quarkus cache.
+        return Uni.createFrom().item(executedAtProvider::now)
+                .chain(executedAtStart -> antlrParseTreeGenerator.generate(formula)
+                        .chain(generatorResult ->
+                                Uni.createFrom().item(() -> {
+                                    final EvalVisitor visitor = new EvalVisitor(executionWrapper, structuredData, numericalContext);
+                                    final Result result = visitor.visit(generatorResult.parseTree());
+                                    if (result == null) {
+                                        throw new IllegalStateException("Should not be null - a response is expected");
+                                    }
+                                    final List<ElementExecution> elementExecutions = executionWrapper.executions();
+                                    final ExecutedAtEnd executedAtEnd = executedAtProvider.now();
+                                    return new ExecutionResult(result,
+                                            elementExecutions,
+                                            new ExecutionProcessedIn(executedAtStart, executedAtEnd));
+                                })
+                        ))
                 .onFailure(Exception.class)
                 .transform(ExecutionException::new);
     }
