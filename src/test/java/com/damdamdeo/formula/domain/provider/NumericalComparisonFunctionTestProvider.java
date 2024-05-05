b1572a2fc77558e4cf9e105a1c1779548c08541f
damien <damien.clementdhuart@gmail.com>
2024-05-05 13:52:23 +0200

    avoid duplicate

--- src/test/java/com/damdamdeo/formula/domain/provider/NumericalComparisonFunctionTestProvider.java
+++ src/test/java/com/damdamdeo/formula/domain/provider/NumericalComparisonFunctionTestProvider.java
@@ -35,21 +35,20 @@ public class NumericalComparisonFunctionTestProvider {
     }
 
     public static Stream<Arguments> provideCommonResponses() {
-        return Stream.of(
-                Arguments.of(new Value("#NA!"), new Value("260"), new Value("#NA!")),
-                Arguments.of(new Value("#REF!"), new Value("260"), new Value("#REF!")),
-                Arguments.of(new Value("#NUM!"), new Value("260"), new Value("#NUM!")),
-                Arguments.of(new Value("#DIV/0!"), new Value("260"), new Value("#DIV/0!")),
-                Arguments.of(new Value("660"), new Value("#NA!"), new Value("#NA!")),
-                Arguments.of(new Value("660"), new Value("#REF!"), new Value("#REF!")),
-                Arguments.of(new Value("660"), new Value("#NUM!"), new Value("#NUM!")),
-                Arguments.of(new Value("660"), new Value("#DIV/0!"), new Value("#DIV/0!")),
-                Arguments.of(new Value("azerty"), new Value("260"), new Value("#NUM!")),
-                Arguments.of(new Value("660"), new Value("azerty"), new Value("#NUM!")),
-                Arguments.of(new Value("#NA!"), new Value("#NA!"), new Value("#NA!")),
-                Arguments.of(new Value("#REF!"), new Value("#REF!"), new Value("#REF!")),
-                Arguments.of(new Value("#NUM!"), new Value("#NUM!"), new Value("#NUM!")),
-                Arguments.of(new Value("#DIV/0!"), new Value("#DIV/0!"), new Value("#DIV/0!"))
+        return Stream.concat(
+                Stream.of(
+                        Arguments.of(new Value("#NA!"), new Value("260"), new Value("#NA!")),
+                        Arguments.of(new Value("#REF!"), new Value("260"), new Value("#REF!")),
+                        Arguments.of(new Value("#NUM!"), new Value("260"), new Value("#NUM!")),
+                        Arguments.of(new Value("#DIV/0!"), new Value("260"), new Value("#DIV/0!")),
+                        Arguments.of(new Value("660"), new Value("#NA!"), new Value("#NA!")),
+                        Arguments.of(new Value("660"), new Value("#REF!"), new Value("#REF!")),
+                        Arguments.of(new Value("660"), new Value("#NUM!"), new Value("#NUM!")),
+                        Arguments.of(new Value("660"), new Value("#DIV/0!"), new Value("#DIV/0!")),
+                        Arguments.of(new Value("azerty"), new Value("260"), new Value("#NUM!")),
+                        Arguments.of(new Value("660"), new Value("azerty"), new Value("#NUM!"))
+                ),
+                FunctionTestProvider.provideCommonResponses()
         );
     }
 
